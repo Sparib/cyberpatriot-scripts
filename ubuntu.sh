39ac04f87ed999f16bb49a397a23cf0263dd42e1
Sparib <me@sparib.com>
2023-11-30 08:52:35 -0500

    Does some things

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+# CURRENT_USERS=$(awk -F: '($3>=1000)&&($1!="nobody"){print $1}' /etc/passwd)
+# WANTED_USERS=$(cat ~/Desktop/)
 whoami
+echo "$SUDO_USER"
+echo "$HOME/Desktop"
+logname
+
