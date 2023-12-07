70c236974bf1cd7c39265e54b9bea5f962983158
Sparib <me@sparib.com>
2023-12-07 08:19:42 -0500

    Add test for execute (this one actually matters)

--- ubuntu-tests.py
+++ ubuntu-tests.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
-from ubuntu import prependHome
+from ubuntu import prependHome, execute
 import unittest
+from unittest.mock import patch
 
 class TestStuff(unittest.TestCase):
     def test_home(self):
         self.assertEqual(prependHome(""), "/home/sparib/")
+
+    @patch('builtins.input', return_value="n")
+    def test_no(self, _):
+        self.assertEqual(execute("test_command"), None)
+
+    @patch('builtins.input', return_value="y")
+    def test_yes(self, _):
+        self.assertEqual(execute("echo", "test"), "test\n")
+
 
 if __name__ == "__main__":
     unittest.main()
